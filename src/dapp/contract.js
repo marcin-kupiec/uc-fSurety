a91be30f08c73db61eb8fb4b252412e663abb072
marcin-kupiec <marcin.kupiec@brainly.com>
2022-08-11 22:03:47 +0200

    managed to connect dapp with contract

--- src/dapp/contract.js
+++ src/dapp/contract.js
@@ -1,56 +1,68 @@
+import FlightSuretyData from '../../build/contracts/FlightSuretyData.json';
 import FlightSuretyApp from '../../build/contracts/FlightSuretyApp.json';
 import Config from './config.json';
 import Web3 from 'web3';
 
 export default class Contract {
-    constructor(network, callback) {
+  constructor(network, callback) {
 
-        let config = Config[network];
-        this.web3 = new Web3(new Web3.providers.HttpProvider(config.url));
-        this.flightSuretyApp = new this.web3.eth.Contract(FlightSuretyApp.abi, config.appAddress);
-        this.initialize(callback);
-        this.owner = null;
-        this.airlines = [];
-        this.passengers = [];
-    }
+    let config = Config[network];
+    this.web3 = new Web3(new Web3.providers.HttpProvider(config.url));
+    this.flightSuretyData = new this.web3.eth.Contract(FlightSuretyData.abi, config.dataAddress);
+    this.flightSuretyApp = new this.web3.eth.Contract(FlightSuretyApp.abi, config.appAddress);
+    this.appAddress = config.appAddress;
 
-    initialize(callback) {
-        this.web3.eth.getAccounts((error, accts) => {
-           
-            this.owner = accts[0];
+    this.initialize(callback);
+    this.owner = null;
+    this.airlines = [];
+    this.passengers = [];
+  }
 
-            let counter = 1;
-            
-            while(this.airlines.length < 5) {
-                this.airlines.push(accts[counter++]);
-            }
+  initialize(callback) {
+    this.web3.eth.getAccounts((error, accts) => {
 
-            while(this.passengers.length < 5) {
-                this.passengers.push(accts[counter++]);
-            }
+      this.owner = accts[0];
 
-            callback();
+      let counter = 1;
+
+      while (this.airlines.length < 5) {
+        this.airlines.push(accts[counter++]);
+      }
+
+      while (this.passengers.length < 5) {
+        this.passengers.push(accts[counter++]);
+      }
+
+      this.authorizeAppToData()
+        .then(() => callback())
+        .catch(err => {
+          console.log("Could not authorize the App contract");
+          console.log(err);
         });
-    }
+    });
+  }
 
-    isOperational(callback) {
-       let self = this;
-       self.flightSuretyApp.methods
-            .isOperational()
-            .call({ from: self.owner}, callback);
-    }
+  async isOperational() {
+    return this.flightSuretyApp.methods
+      .isOperational()
+      .call({ from: this.owner });
+  }
 
-    fetchFlightStatus(flight, callback) {
-        let self = this;
-        let payload = {
-            airline: self.airlines[0],
-            flight: flight,
-            timestamp: Math.floor(Date.now() / 1000)
-        } 
-        self.flightSuretyApp.methods
-            .fetchFlightStatus(payload.airline, payload.flight, payload.timestamp)
-            .send({ from: self.owner}, (error, result) => {
-                callback(error, payload);
-            });
+  async authorizeAppToData() {
+    return this.flightSuretyData.methods.authorizeCaller(this.appAddress).send({ from: this.owner });
+  }
+
+  fetchFlightStatus(flight, callback) {
+    let self = this;
+    let payload = {
+      airline: self.airlines[0],
+      flight: flight,
+      timestamp: Math.floor(Date.now() / 1000),
     }
+    self.flightSuretyApp.methods
+      .fetchFlightStatus(payload.airline, payload.flight, payload.timestamp)
+      .send({ from: self.owner }, (error, result) => {
+        callback(error, payload);
+      });
+  }
 }
